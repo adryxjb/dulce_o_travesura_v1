6d6e9aa07b4060ed19b12a77b3bc46d9f1f3efb1
adryxjb <53943931+adryxjb@users.noreply.github.com>
2021-02-14 17:18:33 -0600

    v1

---
+++ src/app/components/services/registro.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@angular/core";
+@Injectable()
+export class RegistroService {
+  public titulo: string;
+  public nombre: string;
+  public edad: string;
+  public email: string;
+}
